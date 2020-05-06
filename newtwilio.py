9f90cbf4ad7f6d268a620dcee885868c240303aa
saranyavivekanandan <64025983+saranyavivekanandan@users.noreply.github.com>
2020-05-06 18:41:25 +0530

    Update newtwilio.py

--- newtwilio.py
+++ newtwilio.py
@@ -3,13 +3,13 @@ from twilio.rest import Client
 
 account = 'AC2f93079cb2fc82562f1fd2fd45f4e55a'
 auth = '2ad1596f003dfb430f8caa55c18e340e'
-# mycell= '+919940383040'
-# mytwilio = '+12057368558'
-#
-# client = Client(account, auth)
+mycell= '+919940383040'
+mytwilio = '+12057368558'
+
+client = Client(account, auth)
 # new= ''.join(['New Message' for i in range(10)])
 # print((['New Message' for i in range(10)]))
-# mymessage = client.messages.create(to=mycell, from_=mytwilio, body=new)
+mymessage = client.messages.create(to=mycell, from_=mytwilio, body='new')
 
 
 # from twilio.rest import Client
@@ -19,7 +19,7 @@ auth = '2ad1596f003dfb430f8caa55c18e340e'
 # DANGER! This is insecure. See http://twil.io/secure
 # account_sid = 'AC2f93079cb2fc82562f1fd2fd45f4e55a'
 # auth_token = '2ad1596f003dfb430f8caa55c18e340e'
-client = Client(account_sid, auth_token)
+# client = Client(account_sid, auth_token)
 
 call = client.calls.create(
                         url='http://demo.twilio.com/docs/voice.xml',
