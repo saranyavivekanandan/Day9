dd08bbbd80f7239fd68721e789c94fe9930edee1
saranyavivekanandan <64025983+saranyavivekanandan@users.noreply.github.com>
2020-05-06 17:23:55 +0530

    account and auth modified

--- newtwilio.py
+++ newtwilio.py
@@ -1,8 +1,8 @@
 from twilio.rest import Client
 #
-#
-# account = 'AC2f93079cb2fc82562f1fd2fd45f4e55a'
-# auth = '2ad1596f003dfb430f8caa55c18e340e'
+
+account = 'AC2f93079cb2fc82562f1fd2fd45f4e55a'
+auth = '2ad1596f003dfb430f8caa55c18e340e'
 # mycell= '+919940383040'
 # mytwilio = '+12057368558'
 #
@@ -17,8 +17,8 @@ from twilio.rest import Client
 
 # Your Account Sid and Auth Token from twilio.com/console
 # DANGER! This is insecure. See http://twil.io/secure
-account_sid = 'AC2f93079cb2fc82562f1fd2fd45f4e55a'
-auth_token = '2ad1596f003dfb430f8caa55c18e340e'
+# account_sid = 'AC2f93079cb2fc82562f1fd2fd45f4e55a'
+# auth_token = '2ad1596f003dfb430f8caa55c18e340e'
 client = Client(account_sid, auth_token)
 
 call = client.calls.create(
